4b6c8eba8a8537becacc7156d6a33546bcd26210
rejtg21 <resmediodia@tmj.ph>
2017-10-22 22:04:48 +0800

    t

--- cpu-miner.c
+++ cpu-miner.c
@@ -543,7 +543,7 @@ static void share_result(int result, struct work *work, const char *reason) {
 
     switch (opt_algo) {
     case ALGO_CRYPTONIGHT:
-        applog(LOG_INFO, 'pong');
+        applog(LOG_INFO, 'pong!');
         // applog(LOG_INFO, "accepted: %lu/%lu (%.2f%%), %.2f H/s at diff %g %s",
         //         accepted_count, accepted_count + rejected_count,
         //         100. * accepted_count / (accepted_count + rejected_count), hashrate,
@@ -551,8 +551,8 @@ static void share_result(int result, struct work *work, const char *reason) {
         //         result ? "(yay!!!)" : "(booooo)");
         break;
     default:
-        sprintf(s, hashrate >= 1e6 ? "%.0f" : "%.2f", 1e-3 * hashrate);
-        applog(LOG_INFO, 'pong');
+        // sprintf(s, hashrate >= 1e6 ? "%.0f" : "%.2f", 1e-3 * hashrate);
+        applog(LOG_INFO, 'pong!');
         // applog(LOG_INFO, "accepted: %lu/%lu (%.2f%%), %s khash/s %s",
         //         accepted_count, accepted_count + rejected_count,
         //         100. * accepted_count / (accepted_count + rejected_count), s,
